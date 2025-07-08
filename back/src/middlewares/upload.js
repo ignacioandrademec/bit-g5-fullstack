b5e22cd9f9bc30708709885042c0f013b33b6724
Jose Andrade <ignacio.andrademec@gmail.com>
2025-07-07 20:57:10 -0500

    creación de la carpeta middlewares y archivo upload

---
+++ back/src/middlewares/upload.js
@@ -0,0 +1,18 @@
+import multer from "multer";
+import path from path;
+
+const storage = multer.diskStorage({
+    destination: "imagenes",
+    filename: (req, file, cb)=>{
+        const extension = path.extname(file.originalname);
+        const nameWithoutExtension = path.basename(file.originalname, extension).
+        replace(/\s+/g,'-').toLowerCase()
+        const timestamp = new Date().toISOString().replace(/[-:.TZ]/g);
+
+        const uniqueName = `${nameWithoutExtension}${timestamp}${extension}`;
+        cb(null, uniqueName);
+
+    },
+})
+
+export const uploadSingleImage = multer({storage}).single('imagen');
