908cab172545d11a11ae71b9ea5719cb09644afb
Jose Andrade <ignacio.andrademec@gmail.com>
2025-07-08 21:35:48 -0500

    proyecto back terminado

--- back/src/ayudas/funciones.js
+++ back/src/ayudas/funciones.js
@@ -1,25 +1,27 @@
-import jwt from 'jsonwebtoken'; // con el metodo de sing creamos el token, con el metodo de payload son los daos que queremos firmar dentro del token, la llave secreta es la llave que solo el servidor conoce el cual es crucial para verificar la autenticidad, y los 30 seg es el tiempo en el que expira el token.
+import jwt from 'jsonwebtoken';
 
 export function generarToken(payload){
     return new Promise((resolver, rechazar)=>{
-        jwt.sing(payload, 'llave secreta', {expiresIn: '30s'},(error,token)=>{
-            if (error) {
+        jwt.sign(payload,'llave secreta', {expiresIn: '30s'},(error,token)=>{
+            if(error){
                 rechazar(error);
-            } else {
+            }else{
                 resolver(token);
             }
         });
     });
 }
 
+
 export function verificarToken(token){
     return new Promise((resolver, rechazar)=>{
-        jwt.verify(token, 'llave secreta', (error, decodificado)=>{
-            if (error) {
+        jwt.verify(token, 'llave secreta',(error, decodificado)=>{
+            if (error){
                 rechazar(error);
-            } else {
+            }else{
                 resolver(decodificado)
             }
         });
     });
 }
+
